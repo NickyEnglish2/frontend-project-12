337342fd8256ee0f6c310e31067beef3228f94a9
NickyEnglish2 <ilya.fomin.99.62@gmail.com>
2025-03-18 12:56:47 +0800

    rollbar settings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,27 @@
 import AppRouter from './AppRouter.jsx';
 import { I18nextProvider } from 'react-i18next';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import i18n from './i18n.js';
 import { addRussianDictionary } from './utilities/censorText.js';
 
 function App() {
+  const rollbarConfig = {
+    accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+  };
+
   addRussianDictionary('ru');
 
   return (
-    <I18nextProvider i18n={i18n} defaultNS={'translation'}>
-      <AppRouter />
-    </I18nextProvider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <I18nextProvider i18n={i18n} defaultNS={'translation'}>
+          <AppRouter />
+        </I18nextProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   )
 }
 
